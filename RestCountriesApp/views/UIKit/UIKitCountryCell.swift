a4e2c3bf843ed894355d2d0b04fd7b3120422949
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2025-07-02 11:29:47 +0700

    load image

--- RestCountriesApp/views/UIKit/UIKitCountryCell.swift
+++ RestCountriesApp/views/UIKit/UIKitCountryCell.swift
@@ -16,7 +16,42 @@ final class UIKitCountryCell: UITableViewCell {
         var content = defaultContentConfiguration()
         content.text = country.name.common
         content.secondaryText = country.name.official
-        
+        content.imageProperties.maximumSize = CGSize(width: 44, height: 44)
         contentConfiguration = content
+        
+        image(for: country) { [weak self] result in
+            switch result {
+            case .success(let data):
+                DispatchQueue.main.async {
+                    content.image = UIImage(data: data)
+                    self?.contentConfiguration = content
+                }
+            case .failure:
+                content.image = UIImage(named: "photo")
+            }
+        }
     }
+    
+    enum Error: Swift.Error {
+        case failedToFetchImage
+    }
+    
+    private func image(for country: CountryEntity, completion: @escaping (Result<Data, Error>) -> Void) {
+        let imageURL = country.flag.png
+        let request = URLRequest(url: imageURL, cachePolicy: .useProtocolCachePolicy)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if error != nil {
+                return
+            }
+            if let data, let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(Error.failedToFetchImage))
+                }
+            }
+        }
+        .resume()
+    }
+    
 }
