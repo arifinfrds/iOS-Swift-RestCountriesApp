466f5949ecf4009eccd15f5fde4dc39083a62757
Arifin Firdaus <arifinfrds.engineer@gmail.com>
2025-07-06 08:09:26 +0700

    Fix layout bug when scrolling

--- RestCountriesApp/views/UIKit/UIKitCountryCell.swift
+++ RestCountriesApp/views/UIKit/UIKitCountryCell.swift
@@ -3,6 +3,7 @@ import UIKit
 final class UIKitCountryCell: UITableViewCell {
     
     static let cellId = "UIKitCountryCell"
+    private var currentCountry: CountryEntity?
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -13,25 +14,33 @@ final class UIKitCountryCell: UITableViewCell {
     }
     
     func configure(with country: CountryEntity) {
+        currentCountry = country
+        
         var content = defaultContentConfiguration()
         content.text = country.name.common
         content.secondaryText = country.name.official
         content.image = UIImage(systemName: "progress.indicator")
         content.imageProperties.maximumSize = CGSize(width: 44, height: 44)
-
         contentConfiguration = content
         
         image(for: country) { [weak self] result in
-            switch result {
-            case .success(let data):
-                DispatchQueue.main.async {
-                    content.image = UIImage(data: data)
-                    self?.contentConfiguration = content
+            guard let self, self.currentCountry == country else { return }
+            
+            DispatchQueue.main.async {
+                var updatedContent = self.defaultContentConfiguration()
+                updatedContent.text = country.name.common
+                updatedContent.secondaryText = country.name.official
+                updatedContent.imageProperties.maximumSize = CGSize(width: 44, height: 44)
+                
+                switch result {
+                case .success(let data):
+                    updatedContent.image = UIImage(data: data)
+                    
+                case .failure:
+                    updatedContent.image = UIImage(named: "xmark.octagon")
                 }
-            case .failure:
-                DispatchQueue.main.async {
-                    content.image = UIImage(named: "xmark.octagon")
-                }
+                
+                self.contentConfiguration = updatedContent
             }
         }
     }
@@ -41,6 +50,13 @@ final class UIKitCountryCell: UITableViewCell {
         let session = URLSession(configuration: .ephemeral)
         let imageLoader = DelayedImageDataLoaderDecorator(decoratee: RemoteImageDataLoader(session: session, url: imageURL))
         imageLoader.load(completion: completion)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        currentCountry = nil
+        contentConfiguration = nil
     }
     
 }
